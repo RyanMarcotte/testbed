65c1406aeb7b5f9664dd7be3767ae6eb9d5298ea
MarcotteR <ryan.marcotte@live.ca>
2016-09-25 14:56:35 -0600

    initial mock

---
+++ MockRQ/MockIQ.Core.Transport/Interfaces/IiQmetrix_SecurityRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockIQ.Core.Transport.Interfaces
+{
+	// ReSharper disable once InconsistentNaming
+	public interface IiQmetrix_SecurityRole
+	{
+	}
+}
---
+++ MockRQ/MockIQ.Core.Transport/iQmetrix_SecurityRoles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MockIQ.Core.Transport.Interfaces;
+
+namespace MockIQ.Core.Transport
+{
+	// ReSharper disable once InconsistentNaming
+	internal class iQmetrix_SecurityRoles : IiQmetrix_SecurityRoles
+	{
+
+	}
+}
