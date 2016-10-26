4d22d99570ae402a51cf9740fffd98fb6b980b6c
Ryan Marcotte <RyanM@iqmetrix.com>
2016-10-26 15:42:25 -0600

    CQS factory

---
+++ CQSDIContainer/CQSDIContainer/Commands/DoSomethingCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CQSDIContainer.Attributes;
+using IQ.Platform.Framework.Common.CQS;
+
+namespace CQSDIContainer.Commands
+{
+	[LogExecutionTimeToConsole]
+	[HandleCommandAsynchronously]
+	public class DoSomethingCommandHandler : ICommandHandler<DoSomethingCommand>
+	{
+		public void Handle(DoSomethingCommand command)
+		{
+			for (int n = 0; n < command.Iterations; ++n)
+				Console.WriteLine(n);
+		}
+	}
+}
---
+++ CQSDIContainer/CQSDIContainer/Commands/ZDoNothingAndDoSomethingCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQSDIContainer.Commands
+{
+	public class ZDoNothingAndDoSomethingCommand : ICommand
+	{
+		public ZDoNothingAndDoSomethingCommand(int numberOfIterations)
+		{
+			NumberOfIterations = numberOfIterations;
+		}
+
+		public int NumberOfIterations { get; }
+	}
+}
