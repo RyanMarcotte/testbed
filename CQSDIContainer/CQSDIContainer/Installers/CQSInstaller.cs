1caff3ed7b87197d546518514b6d2444f37ef62d
Ryan Marcotte <RyanM@iqmetrix.com>
2016-11-19 13:18:50 -0600

    typed factories

--- CQSDIContainer/CQSDIContainer/Installers/CQSInstaller.cs
+++ CQSDIContainer/CQSDIContainer/Installers/CQSInstaller.cs
@@ -13,18 +13,62 @@ using IQ.Platform.Framework.Common.CQS;
 
 namespace CQSDIContainer.Installers
 {
+	public interface ICommandHandlerFactory<TCommand>
+	{
+		ICommandHandler<TCommand> Create();
+		void Release(ICommandHandler<TCommand> instance);
+	}
+
+	public interface IAsyncCommandHandlerFactory<TCommand>
+	{
+		IAsyncCommandHandler<TCommand> Create();
+		void Release(IAsyncCommandHandler<TCommand> instance);
+	}
+
+	public interface IResultCommandHandlerFactory<TCommand, TError>
+	{
+		IResultCommandHandler<TCommand, TError> Create();
+		void Release(IResultCommandHandler<TCommand, TError> instance);
+	}
+
+	public interface IAsyncResultCommandHandlerFactory<TCommand, TError>
+	{
+		IAsyncResultCommandHandler<TCommand, TError> Create();
+		void Release(IAsyncResultCommandHandler<TCommand, TError> instance);
+	}
+
+	public interface IQueryHandlerFactory<TQuery, TResult>
+		where TQuery : IQuery<TResult>
+	{
+		IQueryHandler<TQuery, TResult> Create();
+		void Release(IQueryHandler<TQuery, TResult> instance);
+	}
+
+	public interface IAsyncQueryHandlerFactory<TQuery, TResult>
+		where TQuery : IQuery<TResult>
+	{
+		IAsyncQueryHandler<TQuery, TResult> Create();
+		void Release(IAsyncQueryHandler<TQuery, TResult> instance);
+	}
+
 	public class CQSInstaller : IWindsorInstaller
 	{
 		public void Install(IWindsorContainer container, IConfigurationStore store)
 		{
+			container.Register(Component.For(typeof(ICommandHandlerFactory<>)).AsFactory());
+			container.Register(Component.For(typeof(IAsyncCommandHandlerFactory<>)).AsFactory());
+			container.Register(Component.For(typeof(IResultCommandHandlerFactory<,>)).AsFactory());
+			container.Register(Component.For(typeof(IAsyncResultCommandHandlerFactory<,>)).AsFactory());
+			container.Register(Component.For(typeof(IQueryHandlerFactory<,>)).AsFactory());
+			container.Register(Component.For(typeof(IAsyncQueryHandlerFactory<,>)).AsFactory());
+
 			container
 				.Register(Classes.FromThisAssembly().BasedOn(typeof(ICommandHandler<>)).Unless(t => typeof(IDecorateCommandHandler<>).IsAssignableFrom(t)).WithServiceBase().LifestyleSingleton())
 				.Register(Classes.FromThisAssembly().BasedOn(typeof(IAsyncCommandHandler<>)).WithServiceBase().Unless(t => typeof(IDecorateAsyncCommandHandler<>).IsAssignableFrom(t)).LifestyleSingleton())
 				.Register(Classes.FromThisAssembly().BasedOn(typeof(IResultCommandHandler<,>)).WithServiceBase().Unless(t => typeof(IDecorateResultCommandHandler<,>).IsAssignableFrom(t)).LifestyleSingleton())
 				.Register(Classes.FromThisAssembly().BasedOn(typeof(IAsyncResultCommandHandler<,>)).WithServiceBase().Unless(t => typeof(IDecorateAsyncResultCommandHandler<,>).IsAssignableFrom(t)).LifestyleSingleton())
 				.Register(Classes.FromThisAssembly().BasedOn(typeof(IQueryHandler<,>)).WithServiceBase().Unless(t => typeof(IDecorateQueryHandler<,>).IsAssignableFrom(t)).LifestyleSingleton())
-				.Register(Classes.FromThisAssembly().BasedOn(typeof(IAsyncQueryHandler<,>)).WithServiceBase().Unless(t => typeof(IDecorateAsyncQueryHandler<,>).IsAssignableFrom(t)).LifestyleSingleton())
-				.Register(Classes.FromThisAssembly().BasedOn(typeof(IQueryCacheItemFactory<,>)).WithServiceBase().LifestyleTransient());
+				.Register(Classes.FromThisAssembly().BasedOn(typeof(IAsyncQueryHandler<,>)).WithServiceBase().Unless(t => typeof(IDecorateAsyncQueryHandler<,>).IsAssignableFrom(t)).LifestyleSingleton());
 		}
 	}
 }
