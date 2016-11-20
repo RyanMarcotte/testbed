963d916b058b49d3fd5de4906a1924b1155b30d5
Ryan Marcotte <RyanM@iqmetrix.com>
2016-11-20 14:08:10 -0600

    cleanup

--- CQSDIContainer/CQSDIContainer/Commands/DoNothingCommandHandler.cs
+++ CQSDIContainer/CQSDIContainer/Commands/DoNothingCommandHandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CQSDIContainer.Attributes;
 using IQ.Platform.Framework.Common.CQS;
 
 namespace CQSDIContainer.Commands
@@ -12,7 +11,6 @@ namespace CQSDIContainer.Commands
 	{
 		public void Handle(DoNothingCommand command)
 		{
-			//throw new NotImplementedException();
 			Console.WriteLine("I did nothing");
 		}
 	}
